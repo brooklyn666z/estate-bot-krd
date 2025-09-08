529b70672a6b2c238bed9f111b17b848c16deb31
brooklyn666z <dobro-bro-fake@mail.ru>
2025-09-08 05:35:56 +0300

    new route(fix)

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,3 @@ Route::get('/', function () {
     return ["Silence is gold."];
 });
 
-
-Route::post('/telegram/webhook', [\App\Http\Controllers\WebhookController::class, 'webhook'])
-    ->name('telegram.webhook');
