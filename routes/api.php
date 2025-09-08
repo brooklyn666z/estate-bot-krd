b5895f379fac0e027c79350846195000679b1064
brooklyn666z <dobro-bro-fake@mail.ru>
2025-09-08 05:43:06 +0300

    new route(fix)

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
-use App\Http\Controllers\WebhookController;
-use Illuminate\Support\Facades\Route;
-
-
-
-Route::post('/telegram/webhook', [\App\Http\Controllers\WebhookController::class, 'webhook'])
-    ->name('telegram.webhook');
+//use App\Http\Controllers\WebhookController;
+//use Illuminate\Support\Facades\Route;
+//
+//
+//
+//Route::post('/telegram/webhook', [\App\Http\Controllers\WebhookController::class, 'webhook'])
+//    ->name('telegram.webhook');
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
     return ["Silence is gold."];
 });
 
+
+Route::post('/telegram/webhook', [\App\Http\Controllers\WebhookController::class, 'webhook'])
+    ->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class])
+    ->name('telegram.webhook');
